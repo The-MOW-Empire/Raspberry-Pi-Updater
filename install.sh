64b4702829fe90c34f574bbd2d1f7443d96f0606
EpicEnder56 <72661987+EpicEnder56@users.noreply.github.com>
2020-11-28 12:49:40 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-mkdir /home/pi/pi-updater
-wget -o https://raw.githubusercontent.com/The-MOW-Empire/Raspberry-Pi-Updater/main/.update.sh /home/pi/.update.sh
-wget -o https://raw.githubusercontent.com/The-MOW-Empire/Raspberry-Pi-Updater/main/updater.desktop /home/pi/.config/autostart/updater.desktop
+mkdir /home/pi/.config/autostart # Makes autostart directory
+wget https://raw.githubusercontent.com/The-MOW-Empire/Raspberry-Pi-Updater/main/.update.sh
+wget https://raw.githubusercontent.com/The-MOW-Empire/Raspberry-Pi-Updater/main/updater.desktop # Downloads files
+mv .update.sh /home/pi/.update.sh # Moves files
+chmod +x /home/pi/.update.sh # Makes files executable
+mv updater.desktop /home/pi/.config/autostart/updater.desktop
+chmod +x /home/pi/.config/autostart/updater.desktop
