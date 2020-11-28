2339c89360b0424046a75ddd3d79a1540a417eab
EpicEnder56 <72661987+EpicEnder56@users.noreply.github.com>
2020-11-28 12:26:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
-
+mkdir /home/pi/pi-updater
+wget -o https://raw.githubusercontent.com/The-MOW-Empire/Raspberry-Pi-Updater/main/.update.sh /home/pi/.update.sh
+wget -o https://raw.githubusercontent.com/The-MOW-Empire/Raspberry-Pi-Updater/main/updater.desktop /home/pi/.config/autostart/updater.desktop
